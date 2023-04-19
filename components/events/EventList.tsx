dd24f5ea3b761f3fe8923c46e82a4e676017e245
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-04-19 09:43:42 +0100

    changing the EventList

--- components/events/EventList.tsx
+++ components/events/EventList.tsx
@@ -3,17 +3,21 @@ import EventItem from './EventItem';
 function EventList(props: { events: any; }) {
     const { events } = props;
 
+    if (!events) {
+        return <p>No events found.</p>;
+    }
+
     return (
         <ul className="divide-y divide-gray">
             {events.map((event: any) => (
-            <EventItem 
-                key={event.id}
-                id={event.id} 
-                title={event.title} 
-                location={event.location} 
-                date={event.date} 
-                image={event.image}
-            />
+                <EventItem 
+                    key={event.id}
+                    id={event.id} 
+                    title={event.title} 
+                    location={event.location} 
+                    date={event.date} 
+                    image={event.image}
+                />
             ))}
         </ul>
     );
